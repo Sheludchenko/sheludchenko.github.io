d42ff40fb5861bfbf297a60d73bd39192a6e753e
Oleksiy Sheludchenko <Oleksiy_Sheludchenko@epam.com>
2016-09-15 12:37:50 +0300

    Updated style for Create-VMFromExistingStorageAccount.ps1

--- data/Create-VMFromExistingStorageAccount.ps1
+++ data/Create-VMFromExistingStorageAccount.ps1
@@ -23,52 +23,60 @@ $Network = Get-AzureRmVirtualNetwork -Name $VirtualNetworkName `
                                      -ResourceGroupName $ResourceGroup
 
 Try { 
-    $NetworkSecurityGroup = Get-AzureRmNetworkSecurityGroup -Name "$VMName-NetworkSecurityGroup" `
-                                                            -ResourceGroupName $ResourceGroup `
-                                                            -WarningAction Stop `
-                                                            -ErrorAction Stop 
+    $NetworkSecurityGroup = Get-AzureRmNetworkSecurityGroup `
+        -Name "$VMName-NetworkSecurityGroup" `
+        -ResourceGroupName $ResourceGroup `
+        -WarningAction Stop `
+        -ErrorAction Stop 
 }
 Catch { 
-    $NetworkSecurityGroup = New-AzureRmNetworkSecurityGroup -Name "$VMName-NetworkSecurityGroup" `
-                                                            -ResourceGroupName $ResourceGroup `
-                                                            -Location $Location 
+    $NetworkSecurityGroup = New-AzureRmNetworkSecurityGroup `
+        -Name "$VMName-NetworkSecurityGroup" `
+        -ResourceGroupName $ResourceGroup `
+        -Location $Location 
 }
 
 Try { 
-    $PublicIp = Get-AzureRmPublicIpAddress -Name "$VMName-PublicNetworkInterface-01" `
-                                           -ResourceGroupName $ResourceGroup `
-                                           -WarningAction Stop `
-                                           -ErrorAction Stop 
+    $PublicIp = Get-AzureRmPublicIpAddress `
+        -Name "$VMName-PublicNetworkInterface-01" `
+        -ResourceGroupName $ResourceGroup `
+        -WarningAction Stop `
+        -ErrorAction Stop 
 } 
 Catch { 
-    $PublicIp = New-AzureRmPublicIpAddress -Name "$VMName-PublicNetworkInterface-01" `
-                                           -ResourceGroupName $ResourceGroup `
-                                           -Location $Location `
-                                           -AllocationMethod Static 
+    $PublicIp = New-AzureRmPublicIpAddress `
+        -Name "$VMName-PublicNetworkInterface-01" `
+        -ResourceGroupName $ResourceGroup `
+        -Location $Location `
+        -AllocationMethod Static 
 }
 
 Try { 
-    $Nic = Get-AzureRmNetworkInterface  -Name "$VMName-PrivateNetworkInterface-01" `
-                                        -ResourceGroupName $ResourceGroup 
+    $Nic = Get-AzureRmNetworkInterface `
+        -Name "$VMName-PrivateNetworkInterface-01" `
+        -ResourceGroupName $ResourceGroup 
 } 
 Catch { 
-    $Nic =  New-AzureRmNetworkInterface -Name "$VMName-PrivateNetworkInterface-01" `
-                                        -ResourceGroupName $ResourceGroup `
-                                        -Location $Location `
-                                        -Subnet $Network.Subnets[0] `
-                                        -PublicIpAddress $PublicIp `
-                                        -NetworkSecurityGroup $NetworkSecurityGroup
+    $Nic =  New-AzureRmNetworkInterface `
+        -Name "$VMName-PrivateNetworkInterface-01" `
+        -ResourceGroupName $ResourceGroup `
+        -Location $Location `
+        -Subnet $Network.Subnets[0] `
+        -PublicIpAddress $PublicIp `
+        -NetworkSecurityGroup $NetworkSecurityGroup
 }
 
-$VMConfig = New-AzureRmVMConfig -VMName $VMName `
-                                -VMSize $VMSize `
-                                -Verbose | `
+$VMConfig = New-AzureRmVMConfig `
+                -VMName $VMName `
+                -VMSize $VMSize `
+                -Verbose | `
             Add-AzureRmVMNetworkInterface -Id $Nic.Id | `
-            Set-AzureRmVMOSDisk -VhdUri $StorageAccountURI `
-                                -Name "$($VMName)OSDisk" `
-                                -CreateOption attach `
-                                -Windows `
-                                -Caching ReadWrite
+            Set-AzureRmVMOSDisk `
+                -VhdUri $StorageAccountURI `
+                -Name "$($VMName)OSDisk" `
+                -CreateOption attach `
+                -Windows `
+                -Caching ReadWrite
 
 New-AzureRmVM -ResourceGroupName $ResourceGroup `
               -Location $Location `
